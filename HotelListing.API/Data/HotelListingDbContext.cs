e254626091f1317c94acf03b692c2d8d2125419f
fabiovalinhos <fabiovalinhos@gmail.com>
2024-03-27 23:25:34 -0300

    Criação do seed - mas tem um erro

--- HotelListing.API/Data/HotelListingDbContext.cs
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -11,5 +11,52 @@ namespace HotelListing.API.Data
 
         public DbSet<Hotel> Hotels { get; set; }
         public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>().HasData(
+                new Country(){
+                    Id = 1,
+                    Name = "Jamaica",
+                    ShortName = "JM"
+                },
+                new Country(){
+                    Id = 2,
+                    Name = "Brasil",
+                    ShortName = "BR"
+                },
+                new Country(){
+                    Id = 3,
+                    Name = "Estados Unidos",
+                    ShortName = "US"
+                }
+            );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel(){
+                    Id = 1,
+                    Name ="Sandals Resort and Spa",
+                    Address = "Negril",
+                    CountryId = 1,
+                    Rating = 4.5
+                },
+                new Hotel(){
+                    Id = 2,
+                    Name ="Guaruja Hotel",
+                    Address = "Guarujá Cidade",
+                    CountryId = 2,
+                    Rating = 3.5
+                },
+                new Hotel(){
+                    Id = 3,
+                    Name ="Tower Power",
+                    Address = "New York",
+                    CountryId = 3,
+                    Rating = 5
+                }
+            );
+        }
     }
 }
