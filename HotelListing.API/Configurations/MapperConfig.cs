c2694384d8a3279355f8497fd4737bb518d07480
Fabio Vilalba <fabio.vilalba@topcode.com.br>
2024-04-17 11:10:05 -0300

    Refatorando o GET método

--- HotelListing.API/Configurations/MapperConfig.cs
+++ HotelListing.API/Configurations/MapperConfig.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using HotelListing.API.Data;
 using HotelListing.API.Models.Country;
+using HotelListing.API.Models.Hotel;
 
 namespace HotelListing.API.Configurations
 {
@@ -10,6 +11,8 @@ namespace HotelListing.API.Configurations
         {
             CreateMap<Country, CreateCountryDto>().ReverseMap();
             CreateMap<Country, GetCountryDto>().ReverseMap();
+            CreateMap<Country, CountryDto>().ReverseMap();
+            CreateMap<Hotel, HotelDto>().ReverseMap();
         }
     }
 }
