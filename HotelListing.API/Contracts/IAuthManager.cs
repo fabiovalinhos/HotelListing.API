b5cf38b6e528b4e63fbddeae9634d1d133749eee
fabiovalinhos <fabiovalinhos@gmail.com>
2024-05-03 22:47:20 -0300

    Adicionando AuthManager para o registro do user

---
+++ HotelListing.API/Contracts/IAuthManager.cs
@@ -0,0 +1,10 @@
+using HotelListing.API.Models.Users;
+using Microsoft.AspNetCore.Identity;
+
+namespace HotelListing.API.Contracts
+{
+    public interface IAuthManager
+    {
+        Task<IEnumerable<IdentityError>> Register(ApiUserDto userDto);
+    }
+}
