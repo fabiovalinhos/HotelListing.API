c76e44412df20d5d1b529d71a40c106fe0c9e1f1
fabiovalinhos <fabiovalinhos@gmail.com>
2024-05-13 20:41:50 -0300

    Terminado o Generate Token

--- HotelListing.API/Repository/AuthManager.cs
+++ HotelListing.API/Repository/AuthManager.cs
@@ -71,13 +71,26 @@ namespace HotelListing.API.Repository
             var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
             var roles = await _userManager.GetRolesAsync(user);
 
-            var roleClaims = roles.Select(x => new Claim(ClaimTypes.Role, x));
+            var roleClaims = roles.Select(x => new Claim(ClaimTypes.Role, x)).ToList();
             var userClaims = await _userManager.GetClaimsAsync(user);
 
             var claims = new List<Claim>
             {
-new Claim(JwtRegisteredClaimNames.Sub, user.Email)
-            };8:17
+                new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim("uid", user.Id),
+            }.Union(roleClaims).Union(userClaims);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JwtSettings:Issuer"],
+                audience: _configuration["JwtSettings:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(Convert.ToInt32(_configuration["JwtSettings:DurationInMinutes"])),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
