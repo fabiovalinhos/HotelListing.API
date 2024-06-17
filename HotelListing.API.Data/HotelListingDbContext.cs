3ac7cbee48d2403c51a2436401cc91a284cb9bf9
Fabio Vilalba <fabio.vilalba@topcode.com.br>
2024-06-17 15:08:35 -0300

    Ajustando o DBContext pois teve divisao de projetos e não era possível criar por exemplo um novo controller

--- HotelListing.API.Data/HotelListingDbContext.cs
+++ HotelListing.API.Data/HotelListingDbContext.cs
@@ -1,13 +1,15 @@
 using HotelListing.API.Data.Configurations;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace HotelListing.API.Data
 {
     public class HotelListingDbContext : IdentityDbContext<ApiUser>
     {
-        public HotelListingDbContext(){}
-        public HotelListingDbContext(DbContextOptions options) : base(options){}
+        public HotelListingDbContext() { }
+        public HotelListingDbContext(DbContextOptions options) : base(options) { }
 
         public DbSet<Hotel> Hotels { get; set; }
         public DbSet<Country> Countries { get; set; }
@@ -21,6 +23,23 @@ namespace HotelListing.API.Data
             modelBuilder.ApplyConfiguration(new CountryConfiguration());
 
             modelBuilder.ApplyConfiguration(new HotelConfiguration());
+        }
+    }
+
+    public class HotelListingDBContextFactory : IDesignTimeDbContextFactory<HotelListingDbContext>
+    {
+        public HotelListingDbContext CreateDbContext(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<HotelListingDbContext>();
+            var conn = config.GetConnectionString("HotelListingDbConnectionString");
+            optionsBuilder.UseNpgsql(conn);
+
+            return new HotelListingDbContext(optionsBuilder.Options);
         }
     }
 }
